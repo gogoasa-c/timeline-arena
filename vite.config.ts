12a631fd4c0e9fbc19f1c7a3ac16c1d1548e6fa6
Cristian Gogoasa <gogoasac16@gmail.com>
2026-05-23 08:34:24 +0300

    fix(config): change repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // Replace 'timeline_arena' with your actual GitHub repo name
-    base: '/timeline_arena/',
+    base: '/timeline-arena/',
 })
